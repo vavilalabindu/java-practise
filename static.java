daba7de95cc995e053ca7401eca7197c271b7b8c
vavilalabindu <123167924+vavilalabindu@users.noreply.github.com>
2023-02-07 22:17:29 +0530

    01-02-23 java practise

---
+++ static.java
@@ -0,0 +1,28 @@
+class first
+{
+
+   public static void main(String args[])
+    { 
+
+   System.out.println("Hello World");
+   first fs=new first();
+   Display(); 
+   fs.greetings();
+    }
+
+   public static void Display()
+    {
+
+     System.out.println("Hello Bindu");    
+    
+    }
+
+     public void greetings()
+    {
+
+     System.out.println("Good Morning"); 
+   
+    }
+
+
+}
