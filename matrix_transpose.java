48c66bf974dfe959ca38a9949b42b2692938c926
vavilalabindu <123167924+vavilalabindu@users.noreply.github.com>
2023-02-14 22:58:25 +0530

    14-02-2023 java practice

---
+++ matrix_transpose.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class matrix_transpose
+{
+public static void main(String args[])
+{
+ 
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter num of rows");
+ int rows=sc.nextInt();
+ System.out.println("enter num of cols");
+ int cols=sc.nextInt(); 
+
+int arr[][]=new int[rows][cols];
+
+System.out.println("enter array values");
+  for(int i=0;i<rows;i++)//to read array values
+   {
+    for(int j=0;j<cols;j++)
+      {
+       arr[i][j]=sc.nextInt();
+      }
+       
+    }
+
+
+System.out.println("the array values are : ");
+   for(int i=0;i<rows;i++)//to print array values
+   {
+    for(int j=0;j<cols;j++)
+      {
+        System.out.print(arr[j][i]+"\t");
+      }
+       System.out.println();
+    }
+
+
+
+}
+
+
+}
